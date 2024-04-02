44f2b3c991155a8537ba605b7495b3413c175c38
Ntesaa24 <notesa.aldinasari24@gmail.com>
2024-04-02 11:42:15 +0700

    getline bagian 4

--- struktur 4/struktur 4.cpp
+++ struktur 4/struktur 4.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-
+#include <string>
 using namespace std;
 
 struct DetailAlamat {
@@ -17,7 +17,11 @@ struct Mahasiswa {
 int main() {
 	Mahasiswa mhs[3]; 
 	for (int i = 0; i < 3; i++) {
-		
+		cout << "Data ke-" << (i + 1) << ":" << endl; 
+		cout << "Nomor Mahasiswa : "; 
+		getline(cin, mhs[i].nim); 
+		cout << "Nama Mahasiswa : "; 
+		getline(cin, mhs, [i].nama); 
 	}
 
 }
